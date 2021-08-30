a02bd445122052b164fcd0eca4bbbce0e372454f
carina tao <carina.tao5@gmail.com>
2021-08-31 00:48:59 +1000

    original code

---
+++ script.js
@@ -0,0 +1,68 @@
+// css class for different card image
+const CARD_TECHS = [
+  'html5',
+  'css3',
+  'js',
+  'sass',
+  'nodejs',
+  'react',
+  'linkedin',
+  'heroku',
+  'github',
+  'aws'
+];
+
+// only list out some of the properties,
+// add more when needed
+const game = {
+  score: 0,
+  level: 1,
+  timer: 60,
+  timerDisplay: null,
+  scoreDisplay: null,
+  levelDisplay: null,
+  timerInterval: null,
+  startButton: null
+  // and much more
+};
+
+setGame();
+
+/*******************************************
+/     game process
+/******************************************/
+function setGame() {
+  // register any element in your game object
+  const cards = document.querySelectorAll('.memory-card');
+
+  let hasFlippedCard = false;
+  let lockBoard = false;
+  let firstCard, secondCard;
+  startGame();
+}
+
+function startGame() {
+  
+}
+
+function handleCardFlip() {}
+
+function nextLevel() {}
+
+function handleGameOver() {}
+
+/*******************************************
+/     UI update
+/******************************************/
+function updateScore() {}
+
+function updateTimerDisplay() {}
+
+/*******************************************
+/     bindings
+/******************************************/
+function bindStartButton() {}
+
+function unBindCardClick(card) {}
+
+function bindCardClick() {}
